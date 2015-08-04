9bb01b355a98642f1f7f24306b67813595c3d363
roberkanho <nanchen@Nans-MacBook-Pro.local>
2015-08-04 12:21:20 -0700

    initial commit

---
+++ Codepath 1Tests/Codepath_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Codepath_1Tests.swift
+//  Codepath 1Tests
+//
+//  Created by Nan Chen on 8/3/15.
+//  Copyright (c) 2015 Nan Chen. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Codepath_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
